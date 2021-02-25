553da39cd40a73437aca7618b3c4c5e3ee259cd2
Cof <example&example.com>
2021-02-25 22:30:50 +0100

    expression calculator and converter add

--- calculator/core/handle_data.py
+++ calculator/core/handle_data.py
@@ -1,37 +1,57 @@
-arabic_numbers = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-}
-
-special_roman_numbers = {
-    "IV": 4,
-    "IX": 9,
-    "XC": 90,
-    "CD": 400,
-    "CM": 900
-}
-
-
-def exchange_to_arabic(roman_number: str) -> int:
-    a = 0
-    i = 0
-    while i < len(roman_number):
-        if (len(roman_number) - i) >= 2:
-            try:
-                a += special_roman_numbers[roman_number[i]+roman_number[i+1]]
-                i += 2
-            except KeyError:
-                a += arabic_numbers[roman_number[i]]
-                i += 1
+def convert_expression_to_arabic(expression: str):
+    roman_number = ""
+    arabic_expression = ""
+    for elem in expression:
+        if elem == "+" or elem == "-" or elem == "*" or elem == " ":
+            arabic_expression += exchange_to_arabic(roman_number)
+            arabic_expression += elem
+            roman_number = ""
+        elif elem == "/":
+            arabic_expression += exchange_to_arabic(roman_number)
+            arabic_expression += "//"
+            roman_number = ""
         else:
-            a += arabic_numbers[roman_number[i]]
-            i += 1
+            roman_number += elem
 
-    return a
+    arabic_expression += exchange_to_arabic(roman_number)  # add last number to the expression
+    return arabic_expression
 
 
+def exchange_to_arabic(roman_number: str) -> str:
+    arabic_number = 0
+    index = 0
+
+    number_repository = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+        "IV": 4,
+        "IX": 9,
+        "XC": 90,
+        "CD": 400,
+        "CM": 900
+    }
+
+    while index < len(roman_number):
+        if (len(roman_number) - index) >= 2:
+            try:
+                arabic_number += number_repository[roman_number[index] + roman_number[index + 1]]
+                index += 2
+            except KeyError:
+                arabic_number += number_repository[roman_number[index]]
+                index += 1
+        else:
+            arabic_number += number_repository[roman_number[index]]
+            index += 1
+
+    return str(arabic_number)
+
+
+def get_calculated_expression(arabic_expression: str, roman_expression: str):
+
+    expression_result = str(eval(arabic_expression))
+    return roman_expression + "=" + expression_result
