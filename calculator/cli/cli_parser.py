a6867a234aaebba9928da762877c935c23a4cef1
Cof <example&example.com>
2021-03-02 22:45:16 +0100

    argparser help typos corrected

--- calculator/cli/cli_parser.py
+++ calculator/cli/cli_parser.py
@@ -3,13 +3,14 @@ import argparse
 
 def parse():
     parser = argparse.ArgumentParser(description='Calculate mathematical expression in roman number format.'
-                                                 'The application works with maximum value 3999.',
-                                     epilog='All way lead to rom :)')
+                                                 'The application works with maximum value 3999.'
+                                                 'Input format e.g. I+IV*II --> output I+IV*II=IX',
+                                     epilog='All way lead to roma :)')
 
     parser.add_argument('roman_expression',
                         metavar='Roman expression',
                         type=str,
-                        help='Needs a math expression with roman numbers e.g. I+IV*2 ')
+                        help='Needs a math expression with roman numbers e.g. I+IV*II ')
 
 
 
